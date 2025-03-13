e1ea66f866cd49c54573b8c8ff4d5ecc16070585
ABDULLAH HAFIZ <ABDULLAH_MH30@OUTLOOK.COM>
2025-03-13 13:04:51 +0300

    Initial Commit

---
+++ CustomComponent/CustomComponentApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomComponentApp.swift
+//  CustomComponent
+//
+//  Created by Abdullah Hafiz on 13/09/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct CustomComponentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
